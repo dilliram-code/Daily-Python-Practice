a1bb9fe7bdf689d67923cd848cd4eef0f52d4d3d
Dilli Ram Chaudhary <chaudharydr23@gmail.com>
2025-08-15 19:06:06 +0545

    create view note func to see the notes kept

--- day7-file_handling/filehandlingProject/filehandling_majorProject.py
+++ day7-file_handling/filehandlingProject/filehandling_majorProject.py
@@ -18,8 +18,44 @@ def initiate_note():
 # initiate_note()
 
 # Keep backup of the note
+
+
 def backup_note():
     shutil.copy(NOTES, BACKUP_NOTES)
     print("Back up created!")
 
-backup_note()
+# backup_note()
+
+# create_note
+
+
+def create_note():
+    category = input(
+        "Enter your category (like: work, pesonal, ideas):").strip()
+    note = input("Enter your note: ").strip()
+    timestamp = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+
+    with open(NOTES, "a", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerow([timestamp, category, note])
+        print("note created!")
+    backup_note()
+    print("Note saved!")
+
+
+# create_note()
+
+# read note
+def view_notes():
+    with open(NOTES, 'r', newline="") as f:
+        reader = csv.reader(f)
+        next(reader) # skip header
+        notes = list(reader)
+        
+        if notes:
+            for idx, (timestamp, category, note) in enumerate(notes,1):
+                print(f"{idx}.[{timestamp}] {category} {note}")
+        else:
+            print("Notes not found!")
+
+view_notes()
