8066dcc8e7ae3e4196b8d67e148ac120240dbd38
Dilli Ram Chaudhary <chaudharydr23@gmail.com>
2025-08-16 18:51:06 +0545

    create menu for the mini project using dict

---
+++ day8-dictionary/coffee_shop.py
@@ -0,0 +1,12 @@
+print("\n=================Welcome to our Cafe ===================")
+menu = {
+    'milk_tea': 25,
+    'black_tea': 15,
+    'samosa': 20,
+    'chaumin': 50,
+    'momo': 120,
+    'pasta': 60,
+    'pakauda': 10
+}
+def menu():
+    
