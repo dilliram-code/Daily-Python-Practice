61c1bca9bbcca56e359d6b0b1fa22bf8931a28b8
Dilli Ram Chaudhary <chaudharydr23@gmail.com>
2025-08-16 22:29:39 +0545

    project completed

--- day8-dictionary/coffee_shop.py
+++ day8-dictionary/coffee_shop.py
@@ -1,4 +1,3 @@
-print("\n=================Welcome to our Cafe ===================")
 menu = {
     'milk_tea': 25,
     'black_tea': 15,
@@ -8,5 +7,35 @@ menu = {
     'pasta': 60,
     'pakauda': 10
 }
-def menu():
-    
+
+def show_menu():
+    print("\nmilk_tea: 25\nblack_tea: 15\nsamosa: 20\nchaumin: 50\nmomo: 120\npasta: 60\npakauda: 10")
+def your_order():
+    print("\n=================Welcome to our Cafe ===================")
+    print("Following are the today's dishes:")
+    show_menu()
+    total_price = 0
+    order = input("What would you like to have? ").strip()
+    if order in menu:
+        total_price += menu[order]
+        order  = input("Anything else? yes or no ").strip()
+        if order =="yes":
+            show_menu()
+            order = input("What would you like to have? ").strip()
+            total_price += menu[order]
+            print(f"your total bill is Rs. {total_price}.")
+        else:
+            print(f"Your total bill  is Rs. {total_price}.")
+    else:
+        print("Sorry!, this is not available now.")
+        response = input("Would would like to have anything else? yes or no ").strip()
+        response = response.lower()
+        if response == "yes":
+            show_menu()
+            order = input("What would you like to have? ")
+            total_price += menu[order]
+            print(f"Your total bill is Rs.{total_price}.")
+        else:
+            print(f"Your tootal bill  is {total_price}")
+
+your_order()
