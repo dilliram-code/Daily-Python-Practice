8d17331dece0e88f3bc78cf52994ae4421a14a1c
Dilli Ram Chaudhary <chaudharydr23@gmail.com>
2025-08-16 23:05:42 +0545

    intro to dict

---
+++ day8-dictionary/dictionay_practice.py
@@ -0,0 +1,15 @@
+# create dict: first method
+dict1 = dict({"name": "Dilliram"})
+print(type(dict1))
+
+# empty dictionary
+dict2 = dict()
+print(dict2)
+
+# create dict: second method
+fruits = {
+    "first": "apple",
+    "second": "banana",
+    "third": "cherry"
+}
+print(fruits)
