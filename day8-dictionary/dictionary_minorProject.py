8c414e089c9ae9870c28f523ce08626f1ef1c5b2
Dilli Ram Chaudhary <chaudharydr23@gmail.com>
2025-08-17 23:20:59 +0545

    add instructions for the project

---
+++ day8-dictionary/dictionary_minorProject.py
@@ -0,0 +1,12 @@
+print("================== School Management System =================")
+
+'''
+** Instructions to follow **
+- Store student records where the student ID is the key and the details(name, age, grades, contact info, etc.) are the value.
+
+- Features:
+* Add, update, delete student details.
+* Search students by ID or name.
+* Calculate average grades.
+
+'''
