282326648d5d3ee029bc46d18a7fdf1e6397bb01
Dilli Ram Chaudhary <chaudharydr23@gmail.com>
2025-08-21 09:09:13 +0545

    set the code str for the mini project  custom range

---
+++ day10-decorator-generator-etc/range_functionMiniProject.py
@@ -0,0 +1,16 @@
+class MyRange_Class:
+  def __init__(self, start, end):
+    self.start = start
+    self.end = end
+    
+  def __iter__():
+    pass
+  
+class My_Iterator:
+  def __init__(self):
+    pass 
+  def __iter__(self):
+    pass 
+  def __next__(self):
+    pass 
+  
