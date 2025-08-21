84d7b10f544d7459ea978dcf6e6be3ce0534c293
Dilli Ram Chaudhary <chaudharydr23@gmail.com>
2025-08-21 09:29:41 +0545

    project completed: what further? 1. Add more complexity 2. Add the functionality for the reverse range 3. Add the user input

--- day10-decorator-generator-etc/range_functionMiniProject.py
+++ day10-decorator-generator-etc/range_functionMiniProject.py
@@ -3,14 +3,23 @@ class MyRange_Class:
     self.start = start
     self.end = end
     
-  def __iter__():
-    pass
+  def __iter__(self):
+    return My_Iterator(self)    # return the object of this class.
   
 class My_Iterator:
-  def __init__(self):
-    pass 
+  def __init__(self, iterable_obj):  # receive the object from upper class
+    self.iterable = iterable_obj
   def __iter__(self):
-    pass 
+    return self 
   def __next__(self):
-    pass 
+    if self.iterable.start >= self.iterable.end:
+      raise StopIteration
+    
+    current = self.iterable.start
+    self.iterable.start += 1
+    return current
+  
+# range1 = MyRange_Class(1,10)
+for i in MyRange_Class(1,10):
+  print(i)
   
