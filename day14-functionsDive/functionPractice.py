9247dccabfe260689016ee96e5b9e912dcb33787
Dilli Ram Chaudhary <chaudharydr23@gmail.com>
2025-08-26 15:29:53 +0545

    explore: mutable default pitfall

--- day14-functionsDive/functionPractice.py
+++ day14-functionsDive/functionPractice.py
@@ -3,6 +3,9 @@ def iamFunction(*args, **kwargs):
   print("Args:", args)
   print("Kwargs:", kwargs)
 
-iamFunction(1,2,3, fourth = 5, fifth = 6)
+# iamFunction(1,2,3, fourth = 5, fifth = 6)
 
 # Note down: positional arguments are returned in a tuple whereas keyword arguments are returned in a dictionary.
+
+
+# Default argument evaluation & mutable defaults pitfall
