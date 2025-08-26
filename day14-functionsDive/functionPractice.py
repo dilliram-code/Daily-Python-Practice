1415eca35e223bd669c6a4207b88f811135e4a42
Dilli Ram Chaudhary <chaudharydr23@gmail.com>
2025-08-26 15:16:00 +0545

    explore positional and keyword argements

---
+++ day14-functionsDive/functionPractice.py
@@ -0,0 +1,8 @@
+# Posional and keyword arguments example
+def iamFunction(*args, **kwargs):
+  print("Args:", args)
+  print("Kwargs:", kwargs)
+
+iamFunction(1,2,3, fourth = 5, fifth = 6)
+
+# Note down: positional arguments are returned in a tuple whereas keyword arguments are returned in a dictionary.
