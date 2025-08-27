1944d25e9f294e5955bf36b11cc90ca9f8346921
Dilli Ram Chaudhary <chaudharydr23@gmail.com>
2025-08-27 21:26:56 +0545

    set up email entry box

--- PythonProjects/NLP App/app.py
+++ PythonProjects/NLP App/app.py
@@ -17,4 +17,10 @@ class NLPApp:
     heading = Label(self.root, text='NLP App', bg=  '#34495E', fg='white')
     heading.pack(pady=(30,30))
     heading.configure(font = ('verdana', 24, 'bold'))
+    
+    label1= Label(self.root, text="Enter email")
+    label1.pack(pady=(10,10))
+    
+    self.email_input = Entry(self.root, width=40)
+    self.email_input.pack(pady=(5,10), ipady=4)
 nlp = NLPApp()
