5cbd70eb666f034c6603c23b29e58c60f803b94b
Dilli Ram Chaudhary <chaudharydr23@gmail.com>
2025-08-27 20:44:26 +0545

    set up for nlp gui app

---
+++ PythonProjects/NLP App/app.py
@@ -0,0 +1,10 @@
+from tkinter import *
+
+class NLPApp:
+  def __init__(self):
+    
+    # login page load
+    self.root = Tk()
+    self.root.mainloop()
+  
+nlp = NLPApp()
--- PythonProjects/snakeGame.py
+++ PythonProjects/snakeGame.py
@@ -11,7 +11,7 @@ WIDTH, HEIGHT = 800, 600
 CELL_SIZE = 20
 
 # colors
-BLACK = (0,0,0)
+BLACK = (0, 0, 0)
 WHITE = (255, 255, 255)
 GREEN = (0, 255, 0)
 RED = (213, 50, 80)
@@ -20,3 +20,7 @@ BLUE = (50, 153, 213)
 # setup window
 screen = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("Snake Game")
+
+# clock(controls fps)
+clock = pygame.time.Clock()
+SNAKE_SPPED = 12  # more is the number, more will be the speed
