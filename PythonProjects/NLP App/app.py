d0e0d2ef97f5172d26d86b62b819baa235b993d5
Dilli Ram Chaudhary <chaudharydr23@gmail.com>
2025-08-27 21:44:04 +0545

    completed login page

--- PythonProjects/NLP App/app.py
+++ PythonProjects/NLP App/app.py
@@ -29,4 +29,14 @@ class NLPApp:
 
     self.password_input = Entry(self.root, width=40)
     self.password_input.pack(pady=(5, 10), ipady=4)
+    
+    login_btn = Button(self.root, text="Login", width=30, height=2)
+    login_btn.pack(pady=(10, 10))
+    
+    label3 = Label(self.root, text="Not a member?")
+    label3.pack(pady=(20, 10))
+    
+    redirect_btn = Label(self.root, text="Register Now")
+    redirect_btn.pack(pady=(10,10))
+    
 nlp = NLPApp()
