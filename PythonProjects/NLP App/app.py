4a1156ca057fbc10b12af5829d5d095aa5287e28
Dilli Ram Chaudhary <chaudharydr23@gmail.com>
2025-08-27 21:04:46 +0545

    set the background color to the app

--- PythonProjects/NLP App/app.py
+++ PythonProjects/NLP App/app.py
@@ -8,6 +8,7 @@ class NLPApp:
     self.root.title("NLP App")
     # self.root.iconbitmap("resources\image.ico")
     self.root.geometry("300x600")
+    self.root.configure(bg='#34495E')
     self.root.mainloop()
   
 nlp = NLPApp()
