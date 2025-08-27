38839e33985b180af1a815319e57973559edcc79
Dilli Ram Chaudhary <chaudharydr23@gmail.com>
2025-08-27 18:52:27 +0545

    set the screen dim and colors

--- PythonProjects/snakeGame.py
+++ PythonProjects/snakeGame.py
@@ -2,3 +2,17 @@
 import pygame
 import time
 import random
+
+# initialise pygame
+pygame.init()
+
+# screen dimensions
+WIDTH, HEIGHT = 800, 600
+CELL_SIZE = 20
+
+# colors
+BLACK = (0,0,0)
+WHITE = (255, 255, 255)
+GREEN = (0, 255, 0)
+RED = (213, 50, 80)
+BLUE = (50, 153, 213)
