f8dd4c05bb70f8a80e4965b11c73df0abf285c43
Dilli Ram Chaudhary <chaudharydr23@gmail.com>
2025-08-27 21:18:56 +0545

    set up login page gui

--- PythonProjects/NLP App/app.py
+++ PythonProjects/NLP App/app.py
@@ -9,6 +9,12 @@ class NLPApp:
     # self.root.iconbitmap("resources\image.ico")
     self.root.geometry("300x600")
     self.root.configure(bg='#34495E')
+    
+    self.login_gui()
     self.root.mainloop()
-  
+    
+  def login_gui(self):
+    heading = Label(self.root, text='NLP App', bg=  '#34495E', fg='white')
+    heading.pack(pady=(30,30))
+    heading.configure(font = ('verdana', 24, 'bold'))
 nlp = NLPApp()
