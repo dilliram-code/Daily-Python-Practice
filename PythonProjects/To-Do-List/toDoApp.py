01317f47c15c079df8430a64ec2480c8a9aa97cd
Dilli Ram Chaudhary <chaudharydr23@gmail.com>
2025-09-15 23:26:23 +0545

    add the enter task section

--- PythonProjects/To-Do-List/toDoApp.py
+++ PythonProjects/To-Do-List/toDoApp.py
@@ -1,9 +1,19 @@
 tasks = []
 
-def show_menu():
-  print("\n======== To-Do-List =========")
-  print("1. Add Task")
-  print("2. View Tasks")
-  print("3. Remove Task")
-  print("4. Exit")
 
+def show_menu():
+    print("\n======== To-Do-List =========")
+    print("1. Add Task")
+    print("2. View Tasks")
+    print("3. Remove Task")
+    print("4. Exit")
+ 
+
+while True:
+    show_menu()
+    choice = input("Enter your choice.")
+
+    if choice == "1":
+        task = input("Enter task.")
+        tasks.append(task)
+        print("Task added!")
