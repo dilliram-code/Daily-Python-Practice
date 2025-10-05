1cbf88e14f239dc1dad2409842d0cd93e66e4782
dilliram-code <chaudharydr23@gmail.com>
2025-10-05 15:37:51 +0545

    explore: while loop for guessing the right letters

--- PythonProjects/Hangman-Game/hangmanGame.py
+++ PythonProjects/Hangman-Game/hangmanGame.py
@@ -1,5 +1,23 @@
+import random
+
 name = input("What is your name? ")
-print("Welcome, name!")
+print("Welcome, ", name,'!')
 
 words = ['python', 'programming', 'coding', 'javascript', 'learning', 'achievement']
 
+word = random.choice(words)
+# print(word)
+
+right = []
+wrong = []
+
+while True:
+  print("=====================================================")
+  guess = input("Guess the letter: ").lower()
+
+  if guess in word:
+    right.append(guess)
+    print("Right letter: ", right)
+  else:
+    wrong.append(guess)
+    print("Wrong letter: ", guess)
