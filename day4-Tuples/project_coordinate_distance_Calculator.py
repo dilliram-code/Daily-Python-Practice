ad512634bdaf90f44736f0b5b878574a9f0b7bd4
dilliram-code <chaudharydr23@gmail.com>
2025-10-31 15:29:31 +0545

    add: co-ordinate dict for cities

---
+++ day4-Tuples/project_coordinate_distance_Calculator.py
@@ -0,0 +1,8 @@
+'''-----------------PROJECT: CO-ORDINATE DISTANCE CALCULATOR---------------'''
+
+cities = {
+    "Kathmandu": (27.7172, 85.3240),
+    "Pokhara": (28.2096, 83.9856),
+    "Biratnagar": (26.4525, 87.2718),
+    "Lalitpur": (27.6667, 85.3333),
+}
