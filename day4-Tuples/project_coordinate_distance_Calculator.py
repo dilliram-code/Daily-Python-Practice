7472994a0e0ce694c3bf4162f7b9fafcb6e42e2b
dilliram-code <chaudharydr23@gmail.com>
2025-10-31 15:33:28 +0545

    define a function to calculate and return euclidean distance

--- day4-Tuples/project_coordinate_distance_Calculator.py
+++ day4-Tuples/project_coordinate_distance_Calculator.py
@@ -1,8 +1,19 @@
 '''-----------------PROJECT: CO-ORDINATE DISTANCE CALCULATOR---------------'''
-
+import math
 cities = {
     "Kathmandu": (27.7172, 85.3240),
     "Pokhara": (28.2096, 83.9856),
     "Biratnagar": (26.4525, 87.2718),
     "Lalitpur": (27.6667, 85.3333),
 }
+
+def calculate_distance(city1_cord, city2_cord):
+  x1, y1 = city1_cord       # x and y cord for city1
+  x2, y2 = city2_cord       # x and y cord for city2
+
+  distance = (x2 - x1)**2 + (y2 - y1)**2      # Euclidean distance
+  distance_result = math.sqrt(distance)
+
+  return distance_result
+
+
