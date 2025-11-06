4e3728281a6a98980c12f6565b3d6dd4ee9b8928
dilliram-code <chaudharydr23@gmail.com>
2025-11-06 08:40:14 +0545

    explore: add_studend that adds the students info

--- day26-JSON_DeepDive/project_Student_Record_System.py
+++ day26-JSON_DeepDive/project_Student_Record_System.py
@@ -16,3 +16,20 @@ def load_data():
 def save_data(data):
   with open(FILE_NAME, 'w') as f:
     json.dump(data, f, indent=2) 
+  
+# add a new student
+def add_student():
+  name = input("enter your name: ").strip()
+  roll = int(input("enter your roll: ").strip())
+  marks = float(input("enter your marks: ")).strip()
+
+  # a dict that stores above info
+  student = {"name": name, "roll": roll, "marks": marks}
+  students.append(student)                # students -> empty list initially
+  save_data(students)                     # injects students into FILE_NAME 
+  print("✅ student added successfully!")
+
+
+
+
+students = load_data()                # load_data() -> a json file (or a list)
