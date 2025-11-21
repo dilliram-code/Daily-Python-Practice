092be1375b003bc63b3b7eacd64874b53c553141
dilliram-code <chaudharydr23@gmail.com>
2025-11-21 08:59:46 +0545

    feat: add display categories and select a particular one

--- PythonProjects/Expense Tracker/expense-tracker.py
+++ PythonProjects/Expense Tracker/expense-tracker.py
@@ -13,6 +13,26 @@ def get_user_expense():
   expense_amount = float(input("enter your expense amount: ").strip())
   print(f"Your expense name {expense_name}, {expense_amount}")
 
+  expense_categories = [
+    "🍔 Food",
+    "🏠 Home",
+    "💼 Work",
+    "😝 Fun",
+    "✨ Misc" 
+  ]
+
+  # display the categories
+  while True:
+    print("select a category:")
+    for i, category in enumerate(expense_categories):
+      print(f" {i + 1}.{category}")
+    value_range = f"[1 - {len(expense_categories)}]"
+    selected_index = int(input(f"enter a category number {value_range}:").strip())
+
+    if selected_index in range(1,len(expense_categories)+1):
+      break
+    else:
+      print("invalid entry, try again!")
 def save_expense_to_file():
   print(f"Saving User Expense!")
   pass
