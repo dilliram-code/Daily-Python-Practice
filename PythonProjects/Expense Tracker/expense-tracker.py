a0d1e8aa65f630c0c6bd3e4801946fc88142abe3
dilliram-code <chaudharydr23@gmail.com>
2025-11-22 20:20:58 +0545

    feat: read the data in the file line by line and store in expenses list

--- PythonProjects/Expense Tracker/expense-tracker.py
+++ PythonProjects/Expense Tracker/expense-tracker.py
@@ -49,7 +49,13 @@ def summarize_expenses(expense_file_path):
   with open(expense_file_path, "r") as f:
     lines = f.readlines()
     for line in lines:
-      print(line)
-
+      expense_name, expense_amount, expense_category = line.strip().split(",")
+      line_expense = Expense(
+        name=expense_name,
+        amount=expense_amount,
+        category=expense_category
+        )
+      print(line_expense)
+      expenses.append(line_expense)
 if __name__ == "__main__":
   main()
