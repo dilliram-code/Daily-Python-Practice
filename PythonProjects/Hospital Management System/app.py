cbe0e0cd99eecb226105da17b9ce2bf695afca50
dilliram-code <chaudharydr23@gmail.com>
2025-11-24 17:50:26 +0545

    feat: add base class Person with some attributes

--- PythonProjects/Hospital Management System/app.py
+++ PythonProjects/Hospital Management System/app.py
@@ -6,4 +6,16 @@ from datetime import datetime, timedelta
 def generate_id(prefix=""):
   return prefix + uuid.uuid4().hex[:6]
 
-print(generate_id())
+# print(generate_id())              # debugging line
+
+class Person:
+  def __init__(self, name, phone):
+    self.id = generate_id("U-")
+    self.name = name
+    self.phone = phone
+  
+  def __str__(self):
+    return f"{self.name} ({self.phone})"
+
+person1 = Person("Dilli", "9803773533")
+print(person1)
