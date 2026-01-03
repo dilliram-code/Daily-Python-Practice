f7f13aff35bc09453d0d9f2a4be9591248f721f4
dilliram-code <chaudharydr23@gmail.com>
2026-01-03 21:48:32 +0545

    refactor: ATM user interface

--- PythonProjects/ATM Simulation/atm_simulation_mini_project_v3.py
+++ PythonProjects/ATM Simulation/atm_simulation_mini_project_v3.py
@@ -17,45 +17,52 @@ class ATM:
       raise ValueError("Insufficient funds.")
     self.balance = self.balance - amount
 
-# get the input number for the deposit and withdraw functions
-def get_number(prompt):
-  while True:
-    try:
-      number = float(input(prompt))
-      return number
-    except ValueError:
-      print("please enter a valid number.")
 
-def main():
-  atm = ATM()
-  while True:
-    choice = input("choose an option: \n 1.check balance\n 2.deposit amount \n 3.withdraw amount\n 4.exit \n")
-    if choice == '1':
-      balance = atm.check_balance()
-      print(f"Your account balance is ${balance}")
-    elif choice == '2':
-      while True:
-        try:
-          amount = get_number(f"Enter the amount to be deposited.")
-          atm.deposit(amount)
-          print(f"Successfully deposited ${amount}.")
-          break
-        except ValueError as error:
+# create a class to put the UI at one place
+class ATMController:
+  def __init__(self):
+    self.atm = ATM()
+
+  # get the input number for the deposit and withdraw functions
+  def get_number(prompt):
+    while True:
+      try:
+        number = float(input(prompt))
+        return number
+      except ValueError:
+        print("please enter a valid number.")
+
+  def run(self):
+    while True:
+      choice = input("choose an option: \n 1.check balance\n 2.deposit amount \n 3.withdraw amount\n 4.exit \n")
+      if choice == '1':
+        balance = self.atm.check_balance()
+        print(f"Your account balance is ${balance}")
+      elif choice == '2':
+        while True:
+          try:
+            amount = self.get_number(f"Enter the amount to be deposited.")
+            self.atm.deposit(amount)
+            print(f"Successfully deposited ${amount}.")
+            break
+          except ValueError as error:
+              print(error)
+      elif choice == '3':
+        while True:
+          try:
+            amount = self.get_number("Enter the amount to withdraw.")
+            self.atm.withdraw(amount)
+            print(f"Successfully withdrew ${amount}.")
+            break
+          except ValueError as error:
             print(error)
-    elif choice == '3':
-      while True:
-        try:
-          amount = get_number("Enter the amount to withdraw.")
-          atm.withdraw(amount)
-          print(f"Successfully withdrew ${amount}.")
-          break
-        except ValueError as error:
-          print(error)
-    elif choice == '4':
-      print('Thank you for using the ATM.')
-      break 
-    else:
-      print("Invalid input. Please try again!")
+      elif choice == '4':
+        print('Thank you for using the ATM.')
+        break 
+      else:
+        print("Invalid input. Please try again!")
 
-if __name__ == "__main__":
-  main()
+
+  def main():
+     atm = ATMController()
+     atm.run()
