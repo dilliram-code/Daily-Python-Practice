5e3070a7eccd8fb4ca59820423e3f7a749e2d5bf
dilliram-code <chaudharydr23@gmail.com>
2026-01-04 19:28:14 +0545

    design the game flow

---
+++ PythonProjects/Tic Tac Toe Game/tic_tac_toe_v1.py
@@ -0,0 +1,16 @@
+'''----------------Game Flow--------------'''
+# Declare the board
+# Print the board
+# Current player = 'X'
+# Loop
+#     Ask the current player for the move
+#     Store their mark on the board
+#     Print the board
+#     Check the winner
+#     If we have a winner
+#         Print a message
+#         Break
+#     If the board is full
+#         Print a message
+#         Break
+#     Switch the current player
