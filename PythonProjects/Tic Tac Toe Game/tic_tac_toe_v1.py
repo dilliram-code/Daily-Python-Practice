0b612f872a2e2d915074fa14f16e25264a3c564b
dilliram-code <chaudharydr23@gmail.com>
2026-01-04 21:09:56 +0545

    feat: add board fill up logic based on the move taken by the current player

--- PythonProjects/Tic Tac Toe Game/tic_tac_toe_v1.py
+++ PythonProjects/Tic Tac Toe Game/tic_tac_toe_v1.py
@@ -30,3 +30,18 @@ def print_board(board):
     print(f' {row[0]} | {row[1]} | {row[2]} ')
   print(line)
 print_board(board)
+
+
+current_player = 'X'
+while True:
+  print(f"Player {current_player}'s turn.")
+  row = int(input("enter row(0-2)"))
+  column = int(input("enter column(0-2)"))
+
+  if board[row][column] == ' ':
+    board[row][column] = current_player
+  else:
+    print("This spot is already taken.")
+    continue
+
+  print_board(board)
