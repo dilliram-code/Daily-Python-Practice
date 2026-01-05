430db4f248abeab1b3be05a919f111bb0fd10dad
dilliram-code <chaudharydr23@gmail.com>
2026-01-05 18:11:01 +0545

    feat: add logic to check the winner

--- PythonProjects/Tic Tac Toe Game/tic_tac_toe_v1.py
+++ PythonProjects/Tic Tac Toe Game/tic_tac_toe_v1.py
@@ -29,19 +29,42 @@ def print_board(board):
     print(line)
     print(f' {row[0]} | {row[1]} | {row[2]} ')
   print(line)
-print_board(board)
 
+# check winner
+def check_winner(board):
+  # check rows
+  for row in board:
+    if row[0] == row[1] == row[2] != ' ':
+      return True 
+  
+  # check columns
+  for column in range(2):
+    if board[0][column] == board[1][column] == board[2][column] != ' ':
+      return True
+  
+  # check diagonals
+  if board[0][0] == board[1][1] == board[2][2] or \
+  board[0][2] == board[1][1] == board[2][0] != ' ':
+    return True
 
-current_player = 'X'
-while True:
-  print(f"Player {current_player}'s turn.")
-  row = int(input("enter row(0-2)"))
-  column = int(input("enter column(0-2)"))
-
-  if board[row][column] == ' ':
-    board[row][column] = current_player
-  else:
-    print("This spot is already taken.")
-    continue
-
+def main():
   print_board(board)
+
+
+  current_player = 'X'
+  while True:
+    print(f"Player {current_player}'s turn.")
+    row = int(input("enter row(0-2)"))
+    column = int(input("enter column(0-2)"))
+
+    if board[row][column] == ' ':
+      board[row][column] = current_player
+    else:
+      print("This spot is already taken.")
+      continue
+
+    print_board(board)
+
+
+if __name__ == '__main__':
+  main()
