02f374c64ab9efd7a2ce46f294bc84247b196172
dilliram-code <chaudharydr23@gmail.com>
2026-06-12 22:53:38 +0545

    create a class that creates player, secret number and also stores attempts

--- PythonProjects/GuessingGame/app.py
+++ PythonProjects/GuessingGame/app.py
@@ -6,3 +6,16 @@ class Player:
   name: str
   score: int = 0
 
+class GuessNumberGame:
+  total_games_played = 0
+  
+  def __init__(self, player: Player, max_number: int = 10):
+    self.player = player
+    self.max_number = max_number
+    self.secret_number = random.randint(1, max_number)
+    self.attempts = 3
+    GuessNumberGame.total_games_played += 1
+
+player1 = Player("Dilli")
+game = GuessNumberGame(player1)
+print(game.secret_number)
