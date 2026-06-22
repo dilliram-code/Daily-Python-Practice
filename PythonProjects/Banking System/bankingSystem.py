48e03b3c5cae359e53c90e9ef9c2b1b5dc2dc107
dilliram-code <chaudharydr23@gmail.com>
2026-06-22 23:59:30 +0545

    implement bank account

--- PythonProjects/Banking System/bankingSystem.py
+++ PythonProjects/Banking System/bankingSystem.py
@@ -15,3 +15,24 @@ class Transaction:
           f"{self.timestamp.strftime('%Y-%m-%d %H:%M:%S')} |"
           f"{self.transaction_type: <12} | Rs.{self.amount}"
     )
+    
+class BankAccount:
+    """
+    Base class for all bank accounts.
+    """
+
+    bank_name = "Nepal National Bank"
+
+    total_accounts = 0
+
+    def __init__(self, account_number, owner_name, initial_balance=0):
+        self.account_number = account_number
+        self.owner_name = owner_name
+        self.__balance = initial_balance
+        self.transactions = []
+
+        BankAccount.total_accounts += 1
+
+        if initial_balance > 0:
+            self.transactions.append(
+                Transaction("OPEN", initial_balance)
