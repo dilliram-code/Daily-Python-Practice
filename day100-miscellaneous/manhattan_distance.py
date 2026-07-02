27d98ceb281d008e4685655be3fde2de22e7af56
dilliram-code <chaudharydr23@gmail.com>
2026-07-02 15:52:24 +0545

    calculate manhattan distance using numpy

--- day100-miscellaneous/manhattan_distance.py
+++ day100-miscellaneous/manhattan_distance.py
@@ -13,3 +13,14 @@ a = [1,2,3]
 b = [4,5,6]
 
 print(manhattan_distance(a,b))
+
+
+
+# using numpy
+import numpy as np
+
+a = np.array([1,2,3])
+b = np.array([6,7,8])
+
+distance = np.sum(np.abs(a - b))
+print(f"Manhattan distance: {distance}")
