087a9d200b0ff9f7477b8a5d5790708af0e9ad26
dilliram-code <chaudharydr23@gmail.com>
2026-07-29 21:52:57 +0545

    explore: async await with gather

---
+++ day29-AsyncPython/02_async_two.py
@@ -0,0 +1,17 @@
+import asyncio 
+
+
+async def brew(name):
+  print(f"Brewing {name} ...")
+  await asyncio.sleep(2)
+  print(f"{name} is ready...")
+  
+
+async def main():
+  await asyncio.gather(
+    brew("Mashala chai"),
+    brew("Green chai"),
+    brew("Ginger chai"),
+  )
+
+asyncio.run(main())
