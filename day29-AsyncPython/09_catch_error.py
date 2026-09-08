cb17bdb2945674a7e542e3084851216c1d049535
dilliram-code <chaudharydr23@gmail.com>
2026-09-08 23:04:38 +0545

    add: implement error handling in asyncio task group example

---
+++ day29-AsyncPython/09_catch_error.py
@@ -0,0 +1,21 @@
+import asyncio
+
+async def work(name):
+  if name == "B":
+    raise ValueError("Something went wrong.")
+  
+  await asyncio.sleep(2)
+  print(name)
+  
+
+async def main():
+  try:
+    async with asyncio.TaskGroup() as tg:
+      tg.create_task(work("A"))
+      tg.create_task(work("B"))
+      tg.create_task(work("C"))
+      
+  except* ValueError as e:
+    print("Caught:", e)
+  
+asyncio.run(main())
