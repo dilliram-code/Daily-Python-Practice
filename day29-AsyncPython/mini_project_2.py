3e9fe83f742ce97d293f90a14ca00f3646116f73
dilliram-code <chaudharydr23@gmail.com>
2026-09-11 23:51:37 +0545

    fix: handle attendance task cancellation and improve error handling in main function

--- day29-AsyncPython/mini_project_2.py
+++ day29-AsyncPython/mini_project_2.py
@@ -28,11 +28,33 @@ async def fetch_courses():
   raise RuntimeError("Courses API failed!")
 
 async def fetch_attendance(): 
+  
   print("Attendance API: started") 
+  
   try: 
+    
     await asyncio.sleep(5) 
+    
     print("Attendance API: finished") 
+    
     return 87 
+  
   except asyncio.CancelledError: 
+    
     print("Attendance API: CANCELLED") 
+    
     raise
+
+
+async def main(): 
+  try: 
+    async with asyncio.TaskGroup() as tg: 
+      student_task = tg.create_task( fetch_student() ) 
+      courses_task = tg.create_task( fetch_courses() ) 
+      attendance_task = tg.create_task( fetch_attendance() ) 
+  
+  except* RuntimeError as error: 
+    print("\nTaskGroup caught an error:") 
+    print(error) 
+
+asyncio.run(main())
